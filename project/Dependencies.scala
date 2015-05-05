9d318ca83bcf51bd9dfb424c5c21fe97eaea8070
davewgithub <davewgithub@users.noreply.github.com>
2015-05-05 11:04:31 -0700

    Port changes from https://github.com/spark-jobserver/spark-jobserver/commit/61aa364d2bdfa7f810d4c4f0785f7fda245f2a0f.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,9 @@ object Dependencies {
   val excludeJackson = ExclusionRule(organization = "org.codehaus.jackson")
   val excludeNetty = ExclusionRule(organization = "org.jboss.netty")
   val excludeAsm = ExclusionRule(organization = "asm")
+  val excludeQQ = ExclusionRule(organization = "org.scalamacros")
 
-  lazy val typeSafeConfigDeps = "com.typesafe" % "config" % "1.0.0"
+  lazy val typeSafeConfigDeps = "com.typesafe" % "config" % "1.2.1"
   lazy val yammerDeps = "com.yammer.metrics" % "metrics-core" % "2.2.0"
 
   lazy val yodaDeps = Seq(
@@ -18,21 +19,24 @@ object Dependencies {
   lazy val akkaDeps = Seq(
     // Akka is provided because Spark already includes it, and Spark's version is shaded so it's not safe
     // to use this one
-    "com.typesafe.akka" %% "akka-slf4j" % "2.2.4" % "provided",
-    "io.spray" %% "spray-json" % "1.2.5",
+    "com.typesafe.akka" %% "akka-slf4j" % "2.3.4" % "provided",
+    "io.spray" %% "spray-json" % "1.3.1",
     // upgrade version from 1.2.0 to 1.2.1 to solve the logging noise issue
     // details here: https://groups.google.com/forum/#!msg/spray-user/YN2ocRzwhY0/KJOegaDIep8J
     // NOTE: DO NOT upgrade to 1.2.2 since it is incompatiable and will cause tests fail
-    "io.spray" % "spray-can" % "1.2.1",
-    "io.spray" % "spray-routing" % "1.2.1",
-    "io.spray" % "spray-client" % "1.2.1",
+    "io.spray" %% "spray-can" % "1.3.2",
+    "io.spray" %% "spray-routing" % "1.3.2",
+    "io.spray" %% "spray-client" % "1.3.2",
     yammerDeps
   ) ++ yodaDeps
 
   lazy val sparkDeps = Seq(
-    "org.apache.spark" %% "spark-core" % "1.1.1" % "provided",
+    "org.apache.spark" %% "spark-core" % "1.2.1" % "provided" exclude(
+      "io.netty", "netty-all") excludeAll(excludeQQ),
+    "org.apache.spark" %% "spark-sql" % "1.2.1" % "provided" exclude(
+      "io.netty", "netty-all") excludeAll(excludeQQ),
     // Force netty version.  This avoids some Spark netty dependency problem.
-    "io.netty" % "netty" % "3.6.6.Final"
+    "io.netty" % "netty-all" % "4.0.23.Final"
   )
 
   lazy val slickDeps = Seq(
@@ -47,9 +51,9 @@ object Dependencies {
   )
 
   lazy val coreTestDeps = Seq(
-    "org.scalatest" %% "scalatest" % "1.9.1" % "test",
-    "com.typesafe.akka" %% "akka-testkit" % "2.2.4" % "test",
-    "io.spray" % "spray-testkit" % "1.2.0" % "test"
+    "org.scalatest" %% "scalatest" % "2.2.1" % "test",
+    "com.typesafe.akka" %% "akka-testkit" % "2.3.4" % "test",
+    "io.spray" %% "spray-testkit" % "1.3.2" % "test"
   )
 
 
